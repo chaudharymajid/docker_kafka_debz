43cc722a39a177eadeb74a8286f91794945bc1c8
A. Majid Chaudhary <majid.chaudhary@yahoo.co.uk>
2024-09-09 08:07:09 +0000

    before sql server agent

--- scripts/enable_cdc.sql
+++ scripts/enable_cdc.sql
@@ -1,49 +1,30 @@
--- Enable CDC for the database
-USE AdventureWorks2019;
+-- Enable CDC for the sakila database
+USE sakila;
+GO
+
 EXEC sys.sp_cdc_enable_db;
+GO
 
--- Enable CDC for all tables in the Purchasing schema
-DECLARE @schema_name NVARCHAR(128) = N'Purchasing';
-DECLARE @table_name NVARCHAR(128);
-DECLARE @sql NVARCHAR(MAX);
+-- Enable CDC for the actor table
+EXEC sys.sp_cdc_enable_table
+    @source_schema = N'dbo',
+    @source_name = N'actor',
+    @role_name = NULL,
+    @supports_net_changes = 1;
+GO
 
-DECLARE table_cursor CURSOR FOR
-SELECT [name]
-FROM sys.tables
-WHERE [schema_id] = SCHEMA_ID(@schema_name);
+-- Enable CDC for the film table
+EXEC sys.sp_cdc_enable_table
+    @source_schema = N'dbo',
+    @source_name = N'film',
+    @role_name = NULL,
+    @supports_net_changes = 1;
+GO
 
-OPEN table_cursor;
-
-FETCH NEXT FROM table_cursor INTO @table_name;
-
-WHILE @@FETCH_STATUS = 0
-BEGIN
-    SET @sql = N'EXEC sys.sp_cdc_enable_table @source_schema = N''' + @schema_name + ''', @source_name = N''' + @table_name + ''', @role_name = NULL;';
-    EXEC sp_executesql @sql;
-    FETCH NEXT FROM table_cursor INTO @table_name;
-END
-
-CLOSE table_cursor;
-DEALLOCATE table_cursor;
-
--- Enable CDC for all tables in the Sales schema
-SET @schema_name = N'Sales';
-
-DECLARE table_cursor CURSOR FOR
-SELECT [name]
-FROM sys.tables
-WHERE [schema_id] = SCHEMA_ID(@schema_name);
-
-OPEN table_cursor;
-
-FETCH NEXT FROM table_cursor INTO @table_name;
-
-WHILE @@FETCH_STATUS = 0
-BEGIN
-    SET @sql = N'EXEC sys.sp_cdc_enable_table @source_schema = N''' + @schema_name + ''', @source_name = N''' + @table_name + ''', @role_name = NULL;';
-    EXEC sp_executesql @sql;
-    FETCH NEXT FROM table_cursor INTO @table_name;
-END
-
-CLOSE table_cursor;
-DEALLOCATE table_cursor;
+-- Enable CDC for the customer table
+EXEC sys.sp_cdc_enable_table
+    @source_schema = N'dbo',
+    @source_name = N'customer',
+    @role_name = NULL,
+    @supports_net_changes = 1;
+GO
